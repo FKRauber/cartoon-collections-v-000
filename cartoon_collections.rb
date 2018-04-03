a2ce81a080b7947b132318c53b13666e83229159
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-03 22:04:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,9 +25,14 @@ def long_planeteer_calls(calls)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  
+    if snacks.include?(cheese_types)
+      puts snack
+    else
+      return nil
+    end
 
 end
