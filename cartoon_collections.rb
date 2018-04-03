36c10c5064275b83ba2259a366dfa45914089193
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-03 21:41:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,18 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarf_hash = Hash.new
-  dwarves.each_with_index { |dwarf, index| dwarf_hash[dwarf] = index+1 }
-  #puts dwarf_hash.index + ". " + dwarf_hash.dwarf
+  i = 1
+  dwarves.each_with_index do |dwarf, i|
+    num = i+1
+    puts num.to_s + " " + dwarf.to_s
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |planeteer| planeteer.capitalize + "!"}
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
@@ -17,4 +21,6 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+
 end
